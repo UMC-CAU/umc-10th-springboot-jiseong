78d7a9f75f7213cf3332b3f7d2fd137e7af783f7
thepackour <thepackour5610@gmail.com>
2026-04-05 21:34:58 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/umc/storeandmission/StoreandmissionApplication.java
@@ -0,0 +1,13 @@
+package com.umc.storeandmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreandmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreandmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/storeandmission/StoreandmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.storeandmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreandmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
