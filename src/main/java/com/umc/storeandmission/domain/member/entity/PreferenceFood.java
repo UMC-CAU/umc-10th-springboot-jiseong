c36e3428a264b1138860f2efda16a14d6d72706b
thepackour <thepackour5610@gmail.com>
2026-04-07 00:06:05 +0900

    PreferenceFood 작성

--- src/main/java/com/umc/storeandmission/domain/member/entity/PreferenceFood.java
+++ src/main/java/com/umc/storeandmission/domain/member/entity/PreferenceFood.java
@@ -1,4 +1,18 @@
 package com.umc.storeandmission.domain.member.entity;
 
-public class PreferenceFood {
+import com.umc.storeandmission.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class PreferenceFood extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pf_id")
+    private Long pfId;
+
+    @NotBlank
+    private String name;
 }
