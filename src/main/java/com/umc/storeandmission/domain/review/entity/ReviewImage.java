5daaf4b20e5b5500e20cebc75a7b75328181fcc4
thepackour <thepackour5610@gmail.com>
2026-04-07 01:48:38 +0900

    ReviewImage 작성

--- src/main/java/com/umc/storeandmission/domain/review/entity/ReviewImage.java
+++ src/main/java/com/umc/storeandmission/domain/review/entity/ReviewImage.java
@@ -1,4 +1,28 @@
 package com.umc.storeandmission.domain.review.entity;
 
-public class ReviewImage {
+import com.umc.storeandmission.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewImage extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_image_id")
+    private Long reviewImageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @NotBlank
+    private String url;
 }
