a914077a38f7de819f8450664688b98efa1f324f
thepackour <thepackour5610@gmail.com>
2026-04-07 01:46:38 +0900

    Review 작성

--- src/main/java/com/umc/storeandmission/domain/review/entity/Review.java
+++ src/main/java/com/umc/storeandmission/domain/review/entity/Review.java
@@ -1,4 +1,32 @@
 package com.umc.storeandmission.domain.review.entity;
 
-public class Review {
+import com.umc.storeandmission.domain.member.entity.Member;
+import com.umc.storeandmission.domain.mission.entity.Store;
+import com.umc.storeandmission.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    private String content;
 }
