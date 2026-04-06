5975d2423cc2ce8f78ee795f2296085371c34d3a
thepackour <thepackour5610@gmail.com>
2026-04-07 00:09:07 +0900

    Term 작성

--- src/main/java/com/umc/storeandmission/domain/member/entity/Term.java
+++ src/main/java/com/umc/storeandmission/domain/member/entity/Term.java
@@ -1,4 +1,36 @@
 package com.umc.storeandmission.domain.member.entity;
 
-public class Term {
+import com.umc.storeandmission.domain.member.enums.TermType;
+import com.umc.storeandmission.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Term extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "term_id")
+    private Long termId;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TermType termType;
+
+    @NotNull
+    private Integer version;
 }
