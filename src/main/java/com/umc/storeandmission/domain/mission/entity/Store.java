34a5f61108089524aef40a31741b604b881b5cbf
thepackour <thepackour5610@gmail.com>
2026-04-07 01:37:36 +0900

    Store 작성

--- src/main/java/com/umc/storeandmission/domain/mission/entity/Store.java
+++ src/main/java/com/umc/storeandmission/domain/mission/entity/Store.java
@@ -1,4 +1,33 @@
 package com.umc.storeandmission.domain.mission.entity;
 
-public class Store {
+import com.umc.storeandmission.domain.mission.enums.StoreType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store extends Mission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Long storeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private StoreType storeType;
 }
