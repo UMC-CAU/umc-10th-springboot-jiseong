5546874f3652996c7db22a63c08293e330d47515
thepackour <thepackour5610@gmail.com>
2026-04-29 00:47:13 +0900

    MemberController에 getMissionsCompleteCount() 추가

--- src/main/java/com/umc/storeandmission/domain/member/controller/MemberController.java
+++ src/main/java/com/umc/storeandmission/domain/member/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.umc.storeandmission.domain.member.controller;
 
+import com.umc.storeandmission.domain.member.dto.MemberResDTO;
 import com.umc.storeandmission.domain.member.service.MemberService;
 import com.umc.storeandmission.domain.mission.entity.Mission;
 import com.umc.storeandmission.domain.mission.exception.code.MissionSuccessCode;
 import com.umc.storeandmission.domain.mission.service.MissionService;
 import com.umc.storeandmission.global.apiPayload.ApiResponse;
 import com.umc.storeandmission.global.apiPayload.code.BaseSuccessCode;
+import com.umc.storeandmission.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +26,13 @@ public class MemberController {
     ) {
         BaseSuccessCode code = MissionSuccessCode.MISSION_OK;
         return ApiResponse.onSuccess(code, missionService.getMyMissions(/*유저 정보*/));
+    }
+
+    @GetMapping("/me/missions/complete-count")
+    public ApiResponse<MemberResDTO.GetCompleteCount> getMissionsCompleteCount(
+        // 헤더에서 유저 정보 가져와야 함
+    ) {
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, missionService.getMyCompleteCount(/*유저 정보*/));
     }
 }
