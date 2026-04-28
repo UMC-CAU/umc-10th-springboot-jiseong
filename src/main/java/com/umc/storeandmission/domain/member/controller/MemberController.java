c85c201e1b1fa0a12fbe73f91ed2b3c683009940
thepackour <thepackour5610@gmail.com>
2026-04-29 00:41:50 +0900

    MemberController에 getMissions() 추가

--- src/main/java/com/umc/storeandmission/domain/member/controller/MemberController.java
+++ src/main/java/com/umc/storeandmission/domain/member/controller/MemberController.java
@@ -1,4 +1,28 @@
 package com.umc.storeandmission.domain.member.controller;
 
+import com.umc.storeandmission.domain.member.service.MemberService;
+import com.umc.storeandmission.domain.mission.entity.Mission;
+import com.umc.storeandmission.domain.mission.exception.code.MissionSuccessCode;
+import com.umc.storeandmission.domain.mission.service.MissionService;
+import com.umc.storeandmission.global.apiPayload.ApiResponse;
+import com.umc.storeandmission.global.apiPayload.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class MemberController {
+    private final MemberService memberService;
+    private final MissionService missionService;
+
+    @GetMapping("/me/missions")
+    public ApiResponse<List<Mission>> getMissions(
+            // 헤더에서 유저 정보 가져와야 함
+    ) {
+        BaseSuccessCode code = MissionSuccessCode.MISSION_OK;
+        return ApiResponse.onSuccess(code, missionService.getMyMissions(/*유저 정보*/));
+    }
 }
