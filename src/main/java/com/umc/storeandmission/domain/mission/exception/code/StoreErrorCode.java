849c1ca30074628dc70d3e5567f121238ae4f083
thepackour <thepackour5610@gmail.com>
2026-04-28 19:45:30 +0900

    Mission 도메인 성공 및 실패 코드 enum 작성

--- src/main/java/com/umc/storeandmission/domain/mission/exception/code/StoreErrorCode.java
+++ src/main/java/com/umc/storeandmission/domain/mission/exception/code/StoreErrorCode.java
@@ -7,13 +7,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum MissionErrorCode implements BaseErrorCode {
-    MISSION_UNABLE_TO_SUCCESS(HttpStatus.BAD_REQUEST,
-            "MISSION_400_1",
-            "미션을 완료할 수 없습니다."),
-    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "MISSION_404_1",
-            "미션을 찾을 수 없습니다."),
+public enum StoreErrorCode implements BaseErrorCode {
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "STORE_404_1",
+            "가게를 찾을 수 없습니다.")
     ;
 
     private final HttpStatus status;
