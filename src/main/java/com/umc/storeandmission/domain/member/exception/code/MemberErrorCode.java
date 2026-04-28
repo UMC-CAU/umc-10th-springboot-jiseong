82ed28131761f286020d8ac966a505145f00a404
thepackour <thepackour5610@gmail.com>
2026-04-28 19:45:10 +0900

    Member 도메인 성공 및 실패 코드 enum 작성

--- src/main/java/com/umc/storeandmission/domain/member/exception/code/MemberErrorCode.java
+++ src/main/java/com/umc/storeandmission/domain/member/exception/code/MemberErrorCode.java
@@ -1,4 +1,25 @@
 package com.umc.storeandmission.domain.member.exception.code;
 
-public enum MemberErrorCode {
+import com.umc.storeandmission.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+    MEMBER_BAD_REQUEST(HttpStatus.BAD_REQUEST,
+            "MEMBER400_1",
+            "잘못된 정보입니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "MEMBER404_1",
+            "유저를 찾을 수 없습니다."),
+    MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT,
+            "MEMBER409_1",
+            "이미 존재하는 유저입니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
--- src/main/java/com/umc/storeandmission/domain/member/exception/code/MemberSuccessCode.java
+++ src/main/java/com/umc/storeandmission/domain/member/exception/code/MemberSuccessCode.java
@@ -1,4 +1,22 @@
 package com.umc.storeandmission.domain.member.exception.code;
 
-public enum MemberSuccessCode {
+import com.umc.storeandmission.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberSuccessCode implements BaseSuccessCode {
+    MEMBER_OK(HttpStatus.OK,
+            "MEMBER200_1",
+            "성공적으로 유저를 조회했습니다."),
+    MEMBER_CREATED(HttpStatus.CREATED,
+            "MEMBER201_1",
+            "성공적으로 유저를 생성했습니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
