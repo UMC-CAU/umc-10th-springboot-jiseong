6a369a624c6816c8021d9468cb2fdb6a72bb55a3
thepackour <thepackour5610@gmail.com>
2026-04-29 18:01:32 +0900

    GeneralErrorCode에 INTERNAL_SERVER_ERROR 추가

--- src/main/java/com/umc/storeandmission/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/umc/storeandmission/global/apiPayload/code/GeneralErrorCode.java
@@ -19,7 +19,10 @@ public enum GeneralErrorCode implements BaseErrorCode {
             "접근이 금지되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "COMMON404_1",
-            "해당 리소스를 찾을 수 없습니다.")
+            "해당 리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500",
+            "서버 오류가 발생했습니다.")
     ;
 
     private final HttpStatus status;
