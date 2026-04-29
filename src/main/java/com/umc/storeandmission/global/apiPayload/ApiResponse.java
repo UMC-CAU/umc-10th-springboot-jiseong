5bb6b9bc3ab0bc3077f3a7a626644b666df7c3a7
thepackour <thepackour5610@gmail.com>
2026-04-29 16:50:02 +0900

    ApiResponse typo

--- src/main/java/com/umc/storeandmission/global/apiPayload/ApiResponse.java
+++ src/main/java/com/umc/storeandmission/global/apiPayload/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.umc.storeandmission.global.apiPayload;
 
+import com.umc.storeandmission.global.apiPayload.code.BaseErrorCode;
 import com.umc.storeandmission.global.apiPayload.code.BaseSuccessCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,7 +20,7 @@ public class ApiResponse<T> {
         return new ApiResponse<T>(true, code.getCode(), code.getMessage(), result);
     }
 
-    public static <T> ApiResponse<T> onFailure(BaseSuccessCode code, T result) {
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
         return new ApiResponse<T>(false, code.getCode(), code.getMessage(), result);
     }
 }
