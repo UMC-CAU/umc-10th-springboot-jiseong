720769ad3e46724e65f62eecf4752bb184d69d0e
thepackour <thepackour5610@gmail.com>
2026-05-03 21:17:57 +0900

    HomeReqDTO 내부 레코드 및 클래스 삭제
    Removed unused inner classes from HomeReqDTO.

--- src/main/java/com/umc/storeandmission/presentation/home/dto/HomeReqDTO.java
+++ src/main/java/com/umc/storeandmission/presentation/home/dto/HomeReqDTO.java
@@ -1,11 +1,5 @@
 package com.umc.storeandmission.presentation.home.dto;
 
 public class HomeReqDTO {
-
-    public record getHome(
-            Long id
-    ) {}
-
-    public class GetHome {
-    }
+    
 }
