fb2896e346f495d9521fba1dc32724fe92903025
thepackour <thepackour5610@gmail.com>
2026-05-07 08:34:27 +0900

    GetInfo에 storeId 추가 미션 페이지에서 리뷰 남기기 하려면 id가 필요할 것 같음

--- src/main/java/com/umc/storeandmission/domain/mission/dto/MissionResDTO.java
+++ src/main/java/com/umc/storeandmission/domain/mission/dto/MissionResDTO.java
@@ -2,11 +2,13 @@ package com.umc.storeandmission.domain.mission.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class MissionResDTO {
     @Builder
     public record GetInfo(
+        Long storeId,  // 미션 페이지에서 리뷰 남기기 같은거 하려면 id를 알아야 할듯
         String storeName,
         String content,
         Integer reward,
@@ -15,6 +17,5 @@ public class MissionResDTO {
 
     @Builder
     public record GetHome(
-
     ) {}
 }
