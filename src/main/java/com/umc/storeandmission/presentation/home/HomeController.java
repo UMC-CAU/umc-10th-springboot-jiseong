f04d33260d5a5830b303d50c71a97c0aff9065b4
thepackour <thepackour5610@gmail.com>
2026-05-07 10:52:45 +0900

    getHome()에 query string으로 regionId 받도록 설정 미션 진행 현황이 지역 별로 나뉘는 것 같아서 지역 id도 같이 넘기기로 함

--- src/main/java/com/umc/storeandmission/presentation/home/HomeController.java
+++ src/main/java/com/umc/storeandmission/presentation/home/HomeController.java
@@ -4,12 +4,11 @@ import com.umc.storeandmission.application.home.HomeService;
 import com.umc.storeandmission.global.apiPayload.ApiResponse;
 import com.umc.storeandmission.global.apiPayload.code.BaseSuccessCode;
 import com.umc.storeandmission.global.apiPayload.code.GeneralSuccessCode;
-import com.umc.storeandmission.presentation.home.dto.HomeReqDTO;
 import com.umc.storeandmission.presentation.home.dto.HomeResDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +20,11 @@ public class HomeController {
 
     @GetMapping("/v1/home")
     public ApiResponse<HomeResDTO.GetHome> getHome(
+            @RequestParam Long regionId
             /* 헤더로 유저 정보 받아와야 함 */
     ) {
+        Long memberId = 1L;  // 유저 Id 임의로 지정
         BaseSuccessCode code = GeneralSuccessCode.OK;
-        return ApiResponse.onSuccess(code, homeService.getHome(dto));
+        return ApiResponse.onSuccess(code, homeService.getHome(memberId, regionId));
     }
 }
