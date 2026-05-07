76fd0e660bd32bc74bcb9874516084a8805e47db
thepackour <thepackour5610@gmail.com>
2026-05-07 11:40:47 +0900

    MissionRepository 메소드 인자에 @Param 추가 원래 넣었어야 하는데 깜빡함

--- src/main/java/com/umc/storeandmission/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/umc/storeandmission/domain/mission/repository/MissionRepository.java
@@ -8,14 +8,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
     @Query("select count(m) from Mission m " +  // count 함수 사용할 때는 N+1 발생 X
             "join MemberMission mbm on m = mbm.mission " +
             "where mbm.member.memberId = :memberId " +
-            "and m.store.region.regionId = :regionId ")
-    Long countCompletedMissions(Long memberId, Long regionId);
+            "and m.store.region.regionId = :regionId " +
+            "and m.missionStatus = 'COMPLETE' ")
+    Long countCompletedMissions(
+            @Param("memberId") Long memberId,
+            @Param("regionId") Long regionId);
 
     @Query("select m from Mission m " +
             "join MemberMission mbm on m = mbm.mission " +
-            "where mbm.member.memberId = :memberId")
-    Page<Mission> findMissionsByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+            "where mbm.member.memberId = :memberId " +
+            "and m.store.region.regionId = :regionId ")
+    Page<Mission> findMissionsByMemberIdAndRegionId(
+            @Param("memberId") Long memberId,
+            @Param("regionId") Long regionId,
+            Pageable pageable);
 }
