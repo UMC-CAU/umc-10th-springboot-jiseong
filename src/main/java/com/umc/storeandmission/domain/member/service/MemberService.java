53c912bfa439dfe4905ac8b4ce5d3b8b4470bd5e
thepackour <thepackour5610@gmail.com>
2026-05-07 12:00:13 +0900

    MemberService의 getHome() 구현

--- src/main/java/com/umc/storeandmission/domain/member/service/MemberService.java
+++ src/main/java/com/umc/storeandmission/domain/member/service/MemberService.java
@@ -1,4 +1,29 @@
 package com.umc.storeandmission.domain.member.service;
 
+import com.umc.storeandmission.domain.member.dto.MemberResDTO;
+import com.umc.storeandmission.domain.member.entity.Member;
+import com.umc.storeandmission.domain.member.exception.MemberException;
+import com.umc.storeandmission.domain.member.exception.code.MemberErrorCode;
+import com.umc.storeandmission.domain.member.repository.MemberRepository;
+import com.umc.storeandmission.domain.mission.repository.MissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final MissionRepository missionRepository;
+
+    public MemberResDTO.GetHome getHome(Long memberId, Long regionId) {
+        // Member, Region 존재 여부 체크는 HomeService에서 이미 완료
+
+        Member m = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        Long count = missionRepository.countCompletedMissions(memberId, regionId);
+
+        return new MemberResDTO.GetHome(m.getPoint(), count);
+    }
 }
