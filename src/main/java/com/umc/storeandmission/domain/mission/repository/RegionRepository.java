f926decd9d3763477361ab8ad1eaf8a4f5c8b703
thepackour <thepackour5610@gmail.com>
2026-05-07 11:02:42 +0900

    RegionRepository 추가

---
+++ src/main/java/com/umc/storeandmission/domain/mission/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.umc.storeandmission.domain.mission.repository;
+
+import com.umc.storeandmission.domain.mission.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+
+}
