db8479da2e0ebf70b1f75d9184565b53784db758
thepackour <thepackour5610@gmail.com>
2026-05-07 10:36:59 +0900

    MissionRepository에 findMissionsByMemberId() 페이징 조회 구현

--- src/main/java/com/umc/storeandmission/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/umc/storeandmission/domain/mission/repository/MissionRepository.java
@@ -1,7 +1,16 @@
 package com.umc.storeandmission.domain.mission.repository;
 
 import com.umc.storeandmission.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    @Query("select m from Mission m " +
+            "join MemberMission mbm on m = mbm.mission " +
+            "where mbm.member.memberId = :memberId")
+    Page<Mission> findMissionsByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
