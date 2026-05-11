eeeee73d60b86479a13772df786a68165a0af1a9
thepackour <thepackour5610@gmail.com>
2026-05-11 15:29:29 +0900

    ReviewConverter에 toGetMyReviews(), toPagination() 추가

--- src/main/java/com/umc/storeandmission/domain/review/converter/ReviewConverter.java
+++ src/main/java/com/umc/storeandmission/domain/review/converter/ReviewConverter.java
@@ -1,4 +1,31 @@
 package com.umc.storeandmission.domain.review.converter;
 
+import com.umc.storeandmission.domain.review.dto.ReviewResDTO;
+import com.umc.storeandmission.domain.review.entity.Review;
+
+import java.util.List;
+
 public class ReviewConverter {
+    public static ReviewResDTO.GetMyReviews toGetMyReviews(Review review) {
+        return ReviewResDTO.GetMyReviews.builder()
+                .reviewId(review.getReviewId())
+                .storeId(review.getStore().getStoreId())
+                .content(review.getContent())
+                .rating(review.getRating())
+                .build();
+    }
+
+    public static <T> ReviewResDTO.Pagination<T> toPagination(
+            List<T> data,
+            Boolean hasNext,
+            String nextCursor,
+            Integer size
+    ) {
+        return ReviewResDTO.Pagination.<T>builder()
+                .data(data)
+                .hasNext(hasNext)
+                .nextCursor(nextCursor)
+                .size(size)
+                .build();
+    }
 }
