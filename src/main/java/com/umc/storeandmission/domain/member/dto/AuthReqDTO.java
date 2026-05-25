7ffaf99579a9ea3ff2ef2bf92cf01391480a9329
thepackour <thepackour5610@gmail.com>
2026-05-25 21:50:11 +0900

    AuthReqDTO.Login 추가

--- src/main/java/com/umc/storeandmission/domain/member/dto/AuthReqDTO.java
+++ src/main/java/com/umc/storeandmission/domain/member/dto/AuthReqDTO.java
@@ -11,6 +11,14 @@ import java.util.List;
 
 public class AuthReqDTO {
 
+    public record Login(
+            @NotBlank(message = "이메일은 필수입니다.")
+            String email,
+
+            @NotBlank(message = "비밀번호는 필수입니다.")
+            String password
+    ) {}
+
     public record Signup(
         @NotBlank(message = "이름은 필수입니다.")
         String name,
