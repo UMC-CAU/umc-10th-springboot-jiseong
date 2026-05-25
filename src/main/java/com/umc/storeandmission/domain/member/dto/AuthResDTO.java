da8fcdaa5f711718eac50279eba2ef4acc126f40
thepackour <thepackour5610@gmail.com>
2026-05-25 21:49:54 +0900

    AuthResDTO.Login 추가

--- src/main/java/com/umc/storeandmission/domain/member/dto/AuthResDTO.java
+++ src/main/java/com/umc/storeandmission/domain/member/dto/AuthResDTO.java
@@ -1,11 +1,12 @@
 package com.umc.storeandmission.domain.member.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public class AuthResDTO {
-
     public record Signup(
             Long memberId
     ) {}
+
+    public record Login(
+            String accessToken
+            // String refreshToken
+    ) {};
 }
